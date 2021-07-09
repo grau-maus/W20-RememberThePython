a3a21430b4c64adccc8b1e82bcf998bbae966f44
josh <joshtupas@gmail.com>
2021-07-08 21:45:36 -0500

    icon fixes WIP

--- react-app/src/components/EditListModal/index.js
+++ react-app/src/components/EditListModal/index.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
+import { ReactSVG } from "react-svg";
 import { Modal } from "../../context/Modal";
 import EditListForm from "./EditListForm";
 import { Button } from "react-bootstrap";
 import styles from "./EditList.module.css"
+import editIcon from "../../images/icons/font-awesome/edit-regular.svg";
 
 function EditListModal({ title, id }) {
   const [showModal, setShowModal] = useState(false);
   return (
     <>
       <Button variant="Light" className={styles.editBtn} onClick={() => setShowModal(true)}>
-        <i className="far fa-edit"></i>
+        {/* <i className="far fa-edit"></i> */}
+        <ReactSVG src={editIcon} wrapper="svg" className={styles.editIcon} />
       </Button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
-          <EditListForm id={id} title={title}/>
+          <EditListForm id={id} title={title} />
         </Modal>
       )}
     </>
